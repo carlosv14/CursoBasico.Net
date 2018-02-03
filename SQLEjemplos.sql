c0b515acbfaa55bc647698db96c1ebab2f7fd056
carlosv14 <carlos.varela@unitec.edu>
2018-02-03 16:14:23 -0600

    Create SQLEjemplos.sql

---
+++ SQLEjemplos.sql
@@ -0,0 +1,8 @@
+select SUBSTRING(e.LoginId, CHARINDEX('\',e.LoginId) + 1,LEN(e.LoginId)) as Empleado, d.Name as Departamento from HumanResources.Employee e left join HumanResources.EmployeeDepartmentHistory dh
+on e.BusinessEntityID = dh.BusinessEntityID inner join HumanResources.Department d
+on dh.DepartmentID = d.DepartmentID;
+
+select e.LoginId as Empleado, d.Name as Departamento from HumanResources.Employee e 
+left join HumanResources.EmployeeDepartmentHistory dh
+on e.BusinessEntityID = dh.BusinessEntityID inner join HumanResources.Department d
+on dh.DepartmentID = d.DepartmentID;

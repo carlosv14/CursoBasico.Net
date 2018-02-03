e85b273008afd2216c04e6963bd79d9af55a02de
carlosv14 <carlos.varela@unitec.edu>
2018-02-03 16:45:52 -0600

    Update SQLEjemplos.sql

--- SQLEjemplos.sql
+++ SQLEjemplos.sql
@@ -6,3 +6,9 @@ select e.LoginId as Empleado, d.Name as Departamento from HumanResources.Employe
 left join HumanResources.EmployeeDepartmentHistory dh
 on e.BusinessEntityID = dh.BusinessEntityID inner join HumanResources.Department d
 on dh.DepartmentID = d.DepartmentID;
+
+select COUNT(*) as 'Cantidad de Empleados', d.Name as Departamento from HumanResources.Employee e 
+left join HumanResources.EmployeeDepartmentHistory dh
+on e.BusinessEntityID = dh.BusinessEntityID inner join HumanResources.Department d
+on dh.DepartmentID = d.DepartmentID 
+group by d.Name

64bce5fc0f492b77826bba4c0102f911950f2653
carlosv14 <carlos.varela@unitec.edu>
2018-02-03 18:02:14 -0600

    Update SQLEjemplos.sql

--- SQLEjemplos.sql
+++ SQLEjemplos.sql
@@ -12,3 +12,22 @@ left join HumanResources.EmployeeDepartmentHistory dh
 on e.BusinessEntityID = dh.BusinessEntityID inner join HumanResources.Department d
 on dh.DepartmentID = d.DepartmentID 
 group by d.Name
+
+                                      
+ Select top 1 r.Total, r.Nombre from 
+(
+select SUM(e.Salary) as Total, d.Name as Nombre from HumanResources.Employee e inner join
+ HumanResources.EmployeeDepartmentHistory dh
+on e.BusinessEntityID = dh.BusinessEntityID inner join 
+HumanResources.Department d on dh.DepartmentID = d.DepartmentID
+group by d.Name
+) r 
+order by r.Total;
+
+Select top 1 r.Total, r.Nombre from 
+(
+select SUM(e.Salary) as Total, d.Name as Nombre from HumanResources.Employee e inner join HumanResources.EmployeeDepartmentHistory dh
+on e.BusinessEntityID = dh.BusinessEntityID inner join HumanResources.Department d on dh.DepartmentID = d.DepartmentID
+group by d.Name
+) r 
+order by r.Total desc
